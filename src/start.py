7781dd045597c8c7e0aee04b92932de74c9b09d3
PingIsFun <pingisfun@protonmail.com>
2022-04-17 21:12:03 +0200

    set logging level to WARN

--- src/start.py
+++ src/start.py
@@ -4,7 +4,7 @@ from bot import bot
 from setup import TOKEN
 
 if __name__ == "__main__":
-    log_level = logging.INFO
+    log_level = logging.WARN
     fmt = (
         "[%(levelname)s] [%(filename)s]:%(lineno)s>[%(funcName)s]"
         " %(asctime)s - %(message)s "
